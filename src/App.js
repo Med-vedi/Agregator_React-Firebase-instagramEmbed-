52dea28c81891cdae745eb62e3da367c86c3b83b
Med-vedi <desmopap@gmail.com>
2020-07-14 16:54:54 +0200

    SIgnUp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,123 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./Post";
+import { db, auth } from "./firebase";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import { Button, Input } from "@material-ui/core";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 const App = () => {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
+
+  const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) => {
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          post: doc.data(),
+        }))
+      );
+    });
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser)=>{
+      if(authUser){
+        //user has logged in
+        console.log(authUser.l);
+        setUser(authUser)
+        if(authUser.displayName){
+          //dont update username 
+        }else{
+          return authUser.updateProfile({
+            displayName: username
+          })
+        }
+        
+      }else{
+        //user has logged out
+        setUser(null)
+      }
+    })
+    return ()=>{
+      //perfom some cleanup action
+      unsubscribe()
+    }
+    
+  }, [user, username])
+
+  const signUp = (e) => {
+    e.preventDefault()
+    auth.createUserWithEmailAndPassword(email, password).catch(error=> alert(error.message))
+
+  };
+
   return (
     <div className="app">
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <form className='app__signup'>
+            <center>
+              <img
+                className="app__headerImage"
+                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
+                alt=""
+              />
+            </center>
+
+            <Input
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type='submit' onClick={signUp}>Sign Up</Button>
+          </form>
+        </div>
+      </Modal>
+      <Button onClick={() => setOpen(true)}>Sign Up</Button>
+
       <div className="app__header">
         <img
           className="app__headerImage"
@@ -16,11 +129,19 @@ const App = () => {
         Note that the development build is not optimized. To create a production
         build, use npm run build.
       </h1>
-      <Post
+      {posts.map(({ id, post }) => (
+        <Post
+          key={id}
+          username={post.username}
+          caption={post.caption}
+          imageUrl={post.imageUrl}
+        />
+      ))}
+      {/* <Post
         username="Vladi"
         caption="WOW its awesome"
         imageUrl="https://scontent-mxp1-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/62012341_450008379089640_176485431659869456_n.jpg?_nc_ht=scontent-mxp1-1.cdninstagram.com&_nc_cat=100&_nc_ohc=s9iBK3-nPtQAX9UXv9o&oh=a3d37d2f20f1b787914ac501687c0c17&oe=5F30E862"
-      />
+      /> */}
       <Post />
       <Post />
     </div>
---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase'
+
+
+// const firebaseConfig = ;
+
+  
+  const firebaseApp = firebase.initializeApp(
+    {
+        apiKey: "AIzaSyC3bkMw2HWfMgKdeDWpGuEcWgv8KzHh9hA",
+        authDomain: "instashop-81814.firebaseapp.com",
+        databaseURL: "https://instashop-81814.firebaseio.com",
+        projectId: "instashop-81814",
+        storageBucket: "instashop-81814.appspot.com",
+        messagingSenderId: "565045228068",
+        appId: "1:565045228068:web:86368d064c02b8862d5eb1",
+        measurementId: "G-J09PB3J276"
+      }
+  )
+
+  const db = firebaseApp.firestore()
+  const auth = firebase.auth()
+  const storage = firebase.storage()
+
+  export {db, auth, storage}
