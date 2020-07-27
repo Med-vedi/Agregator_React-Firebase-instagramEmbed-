a9fc62a173c587526144b3cc0f61fe6f2b2f4afd
Med-vedi <desmopap@gmail.com>
2020-07-27 11:57:55 +0200

    step-2

--- src/ui/theme/index.js
+++
@@ -1,9 +0,0 @@
-import { createMuiTheme } from '@material-ui/core/styles';
-
-const palette = {
-  primary: { main: '#3f51b5' },
-  secondary: { main: '#f50057' }
-};
-const themeName = 'San Marino Razzmatazz Stick Insects';
-
-export default createMuiTheme({ palette, themeName });
