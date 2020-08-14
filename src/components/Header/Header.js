8a71179136c26cefcb88c6fcdd5e3986fedfdd72
Med-vedi <desmopap@gmail.com>
2020-08-14 23:20:06 +0200

    updated header, upscale onClick

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,58 @@
 import React from "react";
 import HeaderMenu from "./HeaderMenu";
+
+import InputBase from "@material-ui/core/InputBase";
+import { makeStyles } from "@material-ui/core/styles";
+
+
+
 import "./Header.css";
 import LoginMenu from "./LoginMenu";
 import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
 
 import { auth } from "../../firebase";
 
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
+
 const Header = ({ user, menuItemClicked }) => {
+  const classes = useStyles();
+
   return (
     <div className="header__container">
       <HeaderMenu menuItemClicked={menuItemClicked} />
       <div className="header__logo">
-        {user ? <h1>Hi, {user.displayName}</h1> : <h1>Guest</h1>}
+        {user ? <h3>Hi, {user.displayName}</h3> : <h1>Guest</h1>}
+      </div>
+      <div className={classes.search}>
+        <InputBase
+          placeholder="Search…"
+          classes={{
+            root: classes.inputRoot,
+            input: classes.inputInput,
+          }}
+          inputProps={{ "aria-label": "search" }}
+        />
       </div>
       {user ? (
         <PauseCircleOutlineIcon color="error" onClick={() => auth.signOut()} />
--- src/components/Header/HeaderMenu.js
+++ src/components/Header/HeaderMenu.js
@@ -1,47 +1,64 @@
 import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
-import PopupState, { bindTrigger, bindMenu } from "material-ui-popup-state";
-import DehazeIcon from "@material-ui/icons/Dehaze";
-
-// -------------TO DO------------
-// category state update issue to fix
+import "./Header.css";
 
 const HeaderMenu = (props) => {
+  const style = {
+    // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    borderRadius: 3,
+    // border: 0,
+    color: "black",
+    height: 48,
+    // padding: '10px 10px',
+    // boxShadow: "0 3px 5px 2px rgba(255, 255, 255, .3)",
+  };
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
   const [category, setCategory] = useState(""); //to arg for prop
 
   const handleMenuItem = (e) => {
     // console.log(e.currentTarget.id);
     setCategory(e.currentTarget.id);
   };
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <div>
-      <PopupState variant="popover" popupId="demo-popup-menu">
-        {(popupState) => (
-          <React.Fragment>
-            <DehazeIcon
-              variant="contained"
-              color="primary"
-              {...bindTrigger(popupState)}
-            >
-              Menu
-            </DehazeIcon>
-            <Menu {...bindMenu(popupState)}>
-              <MenuItem onClick={handleMenuItem} id="shoes">
-                Shoes
-              </MenuItem>
-              <MenuItem onClick={handleMenuItem} id="style">
-                Style
-              </MenuItem>
-              <MenuItem onClick={handleMenuItem} id="other">
-                Other
-              </MenuItem>
-            </Menu>
-          </React.Fragment>
-        )}
-      </PopupState>
+    <div className="simpleMenu">
+      <Button
+        style={style}
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        Menu
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleMenuItem} id="shoes">
+          Shoes
+        </MenuItem>
+        <MenuItem onClick={handleMenuItem} id="style">
+          Style
+        </MenuItem>
+        <MenuItem onClick={handleMenuItem} id="other">
+          Other
+        </MenuItem>{" "}
+      </Menu>
     </div>
   );
 };
-
 export default HeaderMenu;
